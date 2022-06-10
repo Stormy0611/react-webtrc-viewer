364fd52b20134638c0e7847f0715315fc077e590
unknown <devstar0611@gmail.com>
2022-06-10 13:15:47 -0700

    Revert "Finish Frontend"
    This reverts commit d53d2b7b12522dbe3ac2ee614a9b7eefb098744c.

--- src/config/fbConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDMY5ZtTOQjpHiwRht_drkK7VvKDNX4ayQ",
-    authDomain: "fire-99891.firebaseapp.com",
-    projectId: "fire-99891",
-    storageBucket: "fire-99891.appspot.com",
-    messagingSenderId: "791596495998",
-    appId: "1:791596495998:web:5a5f278d8462f9f544542f",
-    measurementId: "G-CFPVDEBRG2"
-};
